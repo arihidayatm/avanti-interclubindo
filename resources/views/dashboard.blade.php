de0792110e8a583175b9278c563cc6f5b4f2ab0e
arihidayatm <muhamadarihidayat@gmail.com>
2024-12-24 11:09:02 +0700

    dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -11,16 +11,17 @@
         <!-- Font Awesome Icons -->
         <script src="https://kit.fontawesome.com/42d5adcbca.js" crossorigin="anonymous"></script>
         <!-- Nucleo Icons -->
-        <link href="../assets/css/nucleo-icons.css" rel="stylesheet" />
-        <link href="../assets/css/nucleo-svg.css" rel="stylesheet" />
+        <link href="./assets/css/nucleo-icons.css" rel="stylesheet" />
+        <link href="./assets/css/nucleo-svg.css" rel="stylesheet" />
         <!-- Popper -->
         <script src="https://unpkg.com/@popperjs/core@2"></script>
         <!-- Main Styling -->
         <link href="../assets/css/soft-ui-dashboard-tailwind.css?v=1.0.5" rel="stylesheet" />
+        <link rel="stylesheet" href="soft-ui-dashboard-tailwind.css" />
 
         <!-- Nepcha Analytics (nepcha.com) -->
         <!-- Nepcha is a easy-to-use web analytics. No cookies and fully compliant with GDPR, CCPA and PECR. -->
-        <script defer data-site="YOUR_DOMAIN_HERE" src="https://api.nepcha.com/js/nepcha-analytics.js"></script>
+        {{-- <script defer data-site="YOUR_DOMAIN_HERE" src="https://api.nepcha.com/js/nepcha-analytics.js"></script> --}}
     </head>
 
     
@@ -879,7 +880,7 @@
                                 document.write(new Date().getFullYear() + ",");
                             </script>
                             made with <i class="fa fa-heart"></i> by
-                            <a href="https://www.creative-tim.com" class="font-semibold text-slate-700" target="_blank">Creative Tim</a>
+                            <a href="#" class="font-semibold text-slate-700" target="_blank">MAH Dev</a>
                             for a better web.
                             </div>
                         </div>
@@ -972,6 +973,7 @@
         </div>
     </body>
 
+    <script src="../path/to/soft-ui-dashboard-tailwind.js"></script>
     <!-- plugin for charts  -->
     <script src="../assets/js/plugins/chartjs.min.js" async></script>
     <!-- plugin for scrollbar  -->
